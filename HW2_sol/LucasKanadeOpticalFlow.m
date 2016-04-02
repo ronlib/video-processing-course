e6483c438b7143838be3bf61d112616efeda68da
Ron <ronll64@gmail.com>
2016-04-02 21:52:30 +0300

    advanced. Still need to loop the process in with different samples

--- HW2_sol/LucasKanadeOpticalFlow.m
+++ HW2_sol/LucasKanadeOpticalFlow.m
@@ -1,28 +1,44 @@
 function [u,v] = LucasKanadeOpticalFlow(I1, I2, WindowSize,MaxIter,NumLevels)
-    p = [0,0];
+    
+    i_resized = cell(1, NumLevels);
+    i_resized{1, NumLevels} = I1;
+    i_resized{2, NumLevels} = I2;
+    
+    for level = [1:NumLevels-1]
+        i_resized{1, NumLevels-level} = imgaussfilt(i_resized{1, NumLevels-level+1});
+        i_resized{1, NumLevels-level} = i_resized{1, NumLevels-level}(1:2:end, 1:2:end);        
+        i_resized{2, NumLevels-level} = imgaussfilt(i_resized{2, NumLevels-level+1}) ;
+        i_resized{2, NumLevels-level} = i_resized{2, NumLevels-level}(1:2:end, 1:2:end);
+    end
+    
+    p = [0; 0];    
 
-    warp(:,:,1) = ones(size(I2))*p(1);
-    warp(:,:,2) = ones(size(I2))*p(2);
-    
-    [ix, iy] = gradient(I2);
-    ixx = sum(sum(imwarp(imfilter(ix.*ix, ones(WindowSize, WindowSize)), -warp)));
-    iyy = sum(sum(imwarp(imfilter(iy.*iy, ones(WindowSize, WindowSize)), -warp)));
-    ixy = sum(sum(imwarp(imfilter(ix.*iy, ones(WindowSize, WindowSize)), -warp)));
-    
-%     If warp is positive, the image is pushed upwards and to the left,
-%     contrary to what we calculated in classb
-    it = imwarp(I2, -warp) - I1;
-    
-    B = [ixx, ixy;
-         ixy, iyy];
-     
-%      Stopped here. Need to calculate B*it. Notice that I need to consider
-%      the window size
-     
-    B_it = [sum(sum(ix.*it))
-     
-%     delta_p = 
-   
-%    reshape(I1, 1,[])'
+    for loop = [1:MaxIter]        
+
+        warp(:,:,1) = ones(size(I2))*p(1);
+        warp(:,:,2) = ones(size(I2))*p(2);
+
+        [ix, iy] = gradient(I2);
+        ixx = sum(sum(imwarp(imfilter(ix.*ix, ones(WindowSize, WindowSize)), warp)));
+        iyy = sum(sum(imwarp(imfilter(iy.*iy, ones(WindowSize, WindowSize)), warp)));
+        ixy = sum(sum(imwarp(imfilter(ix.*iy, ones(WindowSize, WindowSize)), warp)));
+
+
+        it = imwarp(I2, warp) - I1;
+
+        B = [ixx, ixy;
+             ixy, iyy];
+
+
+        B_it = [sum(sum(imfilter(ix.*ix, ones(WindowSize, WindowSize)).*it));
+                sum(sum(imfilter(iy.*iy, ones(WindowSize, WindowSize)).*it))];
+
+        delta_p = -inv(B)*B_it;
+
+        p = p + delta_p;
+
+    %    reshape(I1, 1,[])'
+
+    end
 
 end
