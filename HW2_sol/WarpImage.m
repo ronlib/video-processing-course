4c242244f1ac011c7a8f1efd84052979a60012c7
Ron <ronll64@gmail.com>
2016-04-14 00:42:25 +0300

    added Ori's changes

--- HW2_sol/WarpImage.m
+++ HW2_sol/WarpImage.m
@@ -1,13 +1,11 @@
 function I_warp = WarpImage(I,u,v)
-    u(isnan(u)) = 0;
-    v(isnan(v)) = 0;
-    
-    u(isinf(u)) = 0;
-    v(isinf(v)) = 0;
-    
-    warp(:,:,1) = u;
-    warp(:,:,2) = v;
-    
-    I_warp = imwarp(I, warp);
 
+    [xx,yy] = meshgrid(1:size(I,2),1:size(I,1));
+    newX = xx + u;
+    newY = yy + v;
+    I_warp = interp2(xx,yy,I,newX,newY,'cubic');
+    % Replace holes with the value they had in the original image
+    nanIndx = isnan(I_warp);
+    I_warp(nanIndx) = I(nanIndx);
+    
 end
