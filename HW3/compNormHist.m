25af38fc5ad8f0657ac9034c31f0c62202fe5f91
Ron <ronll64@gmail.com>
2016-04-25 15:49:47 +0300

    made some progress

--- HW3/compNormHist.m
+++ HW3/compNormHist.m
@@ -1,16 +1,18 @@
 function normHist = compNormHist(I,s)
+    x_b = s(1)-s(3);
+    x_e = s(1)+s(3);   
+    y_b = s(2)-s(4);
+    y_e = s(2)+s(4);
+    
+    normHist = zeros(16^3, 1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+%     TODO: find a better implementation
+    for i=[y_b:y_e]
+        for j=[x_b:x_e]
+            color_quantized = fix(I(i,j,1)/16)*256 + fix(I(i,j,2)/16)*16 + fix(I(i,j,3)/16) + 1;
+            normHist(color_quantized) = normHist(color_quantized) + 1;
+        end
+    end
+    
+    normHist = normHist/sum(normHist);
 end
---
+++ HW3/randomStateVectors.m
@@ -0,0 +1,8 @@
+function stateVectors = randomStateVectors(I, s_initial, deviation_params)
+    i = 0;
+    s_new = 
+    while i < 100
+        s_
+    end
+    
+end
