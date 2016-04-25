66f3e50226bc3070dcb39d8833561facc634aaa2
Ron <ronll64@gmail.com>
2016-04-25 18:59:33 +0300

    made some progress

--- HW3/predictParticles.m
+++ HW3/predictParticles.m
@@ -1,13 +1,3 @@
 function S_next = predictParticles(S_next_tag)
-
-
-
-
-
-
-
-
-
-
-
+    S_next = randomStateVectors(S_next_tag, [20, 10, 3, 7, 5, 10]);
 end
--- HW3/randomStateVectors.m
+++ HW3/randomStateVectors.m
@@ -1,8 +1,20 @@
-function stateVectors = randomStateVectors(I, s_initial, deviation_params)
-    i = 0;
-    s_new = 
-    while i < 100
-        s_
+function stateVectors = randomStateVectors(S, deviation_params)
+%     im_size = size(I);
+    i = 2;
+    stateVectors = S;
+        
+    while i <= size(S, 2)
+        stateVectors(:, i) = stateVectors(:, i) + normnd(s_initial, deviation_params);
+        
+%         TODO check if boundries should be checked
+        if 1
+%         if ~(stateVectors(1, i) + stateVectors(3, i) > im_size(2) || stateVectors(1, i) - stateVectors(3, i) < 1 ...
+%             || stateVectors(1, i) + stateVectors(3, i) + stateVectors(5, i) > im_size(2) || stateVectors(1, i) - stateVectors(3, i) + stateVectors(5, i) < 1 ...
+%             || stateVectors(2, i) + stateVectors(4, i) > im_size(1) || stateVectors(2, i) - stateVectors(4, i) < 1 ...
+%             || stateVectors(2, i) + stateVectors(4, i) + stateVectors(6, i) > im_size(1) || stateVectors(2, i) - stateVectors(4, i) + stateVectors(6, i) < 1)
+        
+            i = i + 1;
+        end
     end
     
 end
