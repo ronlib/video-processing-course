0de5ce9343930717137b22c6eb352da30897dc05
Ron <ronll64@gmail.com>
2016-04-28 02:28:49 +0300

    added a TODO

--- HW3/predictParticles.m
+++ HW3/predictParticles.m
@@ -3,6 +3,7 @@ function S_next = predictParticles(S_next_tag)
     for i=[1:size(S_next_tag(2))]
         S_next(1) = S_next(1) + S_next(5);
         S_next(2) = S_next(2) + S_next(6);
-    end    
+    end
     S_next = randomStateVectors(S_next, [7, 5, 2, 4, 3, 3]');
+%     TODO: should check for out of boundary particles?
 end
