f87668ae14f38c8c9a1efac9082daed38cfd1204
Ron <ronll64@gmail.com>
2016-04-29 10:21:36 +0300

    commented a printout

--- HW3/filterParticles.m
+++ HW3/filterParticles.m
@@ -4,10 +4,9 @@ function filtered_S = filterParticles(I,S)
         S(1,:)-S(3,:) < 1 | S(2,:)-S(4,:) < 1 | ...
         S(1,:)+S(3,:) > width | S(2,:)+S(4,:) > height);
     
-%     TODO: remove
-    if ~ isempty(find(f==0))
-        fprintf('filtered out %d values!\n', length(find(f==0)));
-    end
-    
+%     if ~ isempty(find(f==0))
+%         fprintf('filtered out %d particles!\n', length(find(f==0)));
+%     end
+
     filtered_S = S(:, f);
 end
