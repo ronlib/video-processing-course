e20717c1cc0d7eb156c59f1f1c33eaf7f6414521
Ron <ronll64@gmail.com>
2016-05-21 19:42:04 +0300

    Stabilization works

--- final_project/findImagesTransformation.m
+++ final_project/findImagesTransformation.m
@@ -1,13 +1,20 @@
-function T = findImagesTransformation(I1, I2)  
+function [T, outStauts] = findImagesTransformation(I1, I2)
     N = 100;
 
     I1Corners = detectHarrisFeatures(I1);
     I2Corners = detectHarrisFeatures(I2);
-    
+
 %     [I1CornersThreesoms, I2CornersThreesoms] = generateCornerThreesoms(I1Corners, I2Corners, N);
     [featuresI1,validPtsI1] = extractFeatures(I1, I1Corners);
     [featuresI2,validPtsI2] = extractFeatures(I2, I2Corners);
-    
-    
-    
+
+    matchedIndices = matchFeatures(featuresI1, featuresI2);
+%     showMatchedFeatures(I1, I2, validPtsI1(matchedIndices(:,1)), validPtsI2(matchedIndices(:,2)), 'falsecolor');
+
+    [tform,inlierpoints1,inlierpoints2, status] = estimateGeometricTransform(validPtsI2(matchedIndices(:,2)), ...
+        validPtsI1(matchedIndices(:,1)), 'affine');
+
+    T = tform;
+    outStauts = status;
+
 end
--- final_project/runme.m
+++ final_project/runme.m
@@ -1,12 +1,23 @@
 function runme
-    inputVideo = VideoReader(fullfile('INPUT','input2.avi'));
-    
-    firstFrame = rgb2gray(inputVideo.readFrame());
-    
-    % TODO: remove
-    secondFrame = rgb2gray(inputVideo.readFrame());
-    
-    T = findImagesTransformation(firstFrame, secondFrame);
-        
+    inputVideo = vision.VideoFileReader(fullfile('INPUT','input.avi'));
+    outputVideo = vision.VideoFileWriter(fullfile('OUTPUT','stabilized.avi'), ...
+        'FrameRate', inputVideo.info.VideoFrameRate, 'Quality', 50);
+    outputVideo.VideoCompressor = 'MJPEG Compressor';
+
+    stabilizeVideo(inputVideo, outputVideo);
+
+    release(inputVideo);
+    release(outputVideo);
+
+
+%
+%     % TODO: remove
+%     for i=[1,60]
+%         secondFrame = rgb2gray(step(inputVideo));
+%     end
+%
+%     [T, status] = findImagesTransformation(firstFrame, secondFrame);
+
+
 
 end
