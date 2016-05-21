e659da00e1584da4e6dde0330ece0b5072776d54
Ron <ronll64@gmail.com>
2016-05-21 20:43:30 +0300

    Adjusted the directories of the input and output videos

--- final_project/CODE/runme.m
+++ final_project/CODE/runme.m
@@ -1,7 +1,7 @@
 function runme
 
-    inputVideo = vision.VideoFileReader(fullfile('INPUT','input.avi'));
-    outputVideo = vision.VideoFileWriter(fullfile('OUTPUT','stabilized.avi'), ...
+    inputVideo = vision.VideoFileReader(fullfile('..', 'INPUT','input.avi'));
+    outputVideo = vision.VideoFileWriter(fullfile('..', 'OUTPUT','stabilized.avi'), ...
         'FrameRate', inputVideo.info.VideoFrameRate, 'Quality', 50, 'VideoCompressor', 'MJPEG Compressor');
 
     %     Video stabilization
