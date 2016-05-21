16956bc9b3a7ca2c4a0051de3487e06f980c54b1
Ron <ronll64@gmail.com>
2016-05-21 20:03:08 +0300

    Removed unnecessary comments

--- final_project/findImagesTransformation.m
+++ final_project/findImagesTransformation.m
@@ -4,7 +4,6 @@ function [T, outStauts] = findImagesTransformation(I1, I2)
     I1Corners = detectHarrisFeatures(I1);
     I2Corners = detectHarrisFeatures(I2);
 
-%     [I1CornersThreesoms, I2CornersThreesoms] = generateCornerThreesoms(I1Corners, I2Corners, N);
     [featuresI1,validPtsI1] = extractFeatures(I1, I1Corners);
     [featuresI2,validPtsI2] = extractFeatures(I2, I2Corners);
 
--- final_project/runme.m
+++ final_project/runme.m
@@ -1,23 +1,13 @@
 function runme
+
     inputVideo = vision.VideoFileReader(fullfile('INPUT','input.avi'));
     outputVideo = vision.VideoFileWriter(fullfile('OUTPUT','stabilized.avi'), ...
-        'FrameRate', inputVideo.info.VideoFrameRate, 'Quality', 50);
-    outputVideo.VideoCompressor = 'MJPEG Compressor';
+        'FrameRate', inputVideo.info.VideoFrameRate, 'Quality', 50, 'VideoCompressor', 'MJPEG Compressor');
 
+    %     Video stabilization
     stabilizeVideo(inputVideo, outputVideo);
 
     release(inputVideo);
     release(outputVideo);
-
-
-%
-%     % TODO: remove
-%     for i=[1,60]
-%         secondFrame = rgb2gray(step(inputVideo));
-%     end
-%
-%     [T, status] = findImagesTransformation(firstFrame, secondFrame);
-
-
 
 end
