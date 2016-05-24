ce0510689a9ff79e26d2ba9c47e15e36a772b6be
Ron <ronll64@gmail.com>
2016-05-24 17:42:12 +0300

    ResizeVideo.m now creates a video with static background

--- final_project/ResizeVideo.m
+++ final_project/ResizeVideo.m
@@ -1,13 +1,18 @@
 function ResizeVideo (input_file_path, output_file_path)
     inputVid = VideoReader(input_file_path);
-%     outputVid = 	(output_file_path);    
-%     outputVid.Quality = inputVid.Quality;
-    outputVid.FrameRate = inputVid.FrameRate;
-    
-    open(outputVid);
-    while hasFrame(inputVid)
-        tempFrame = imresize(readFrame(inputVid), 0.25);
-        writeVideo(outputVid, tempFrame);
+    outputVid = vision.VideoFileWriter(output_file_path, 'FrameRate', inputVid.FrameRate, 'Quality', 50);
+    outputVid.VideoCompressor = 'MJPEG Compressor';
+
+    rectangleCounter = 0;
+%     firstFrame = imresize(readFrame(inputVid), 0.5);
+    firstFrame = readFrame(inputVid);
+    while hasFrame(inputVid) && rectangleCounter < 300
+        currentFrame = firstFrame;
+
+        tempFrame = insertShape(currentFrame, 'FilledRectangle', [rectangleCounter, 150, 40, 100], 'Color', 'black', 'Opacity', 1);
+        rectangleCounter = rectangleCounter+1;
+        step(outputVid, tempFrame);
     end
-    close(outputVid);
+    release(outputVid);
+%     close(inputVid);
 end
