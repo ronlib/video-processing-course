ff87a99ace2037a0df03e7893c5e755a92641ef5
Ron <ronll64@gmail.com>
2016-06-24 00:42:31 +0300

    added Ori's changes to stabilization step

--- final_project/CODE/findImagesTransformation.m
+++ final_project/CODE/findImagesTransformation.m
@@ -1,8 +1,14 @@
 function [T, outStauts] = findImagesTransformation(I1, I2)
     N = 100;
 
-    I1Corners = detectHarrisFeatures(I1);
-    I2Corners = detectHarrisFeatures(I2);
+    %I1Corners = detectHarrisFeatures(I1);
+    %I2Corners = detectHarrisFeatures(I2);
+    %I1Corners = detectSURFFeatures(I1);
+    %I2Corners = detectSURFFeatures(I2);
+    I1Corners = detectSURFFeatures(I1, 'NumOctaves', 2, 'NumScaleLevels', 3);
+    I1Corners = I1Corners.selectStrongest( min( 200, length(I1Corners) ));
+    I2Corners = detectSURFFeatures(I2, 'NumOctaves', 2, 'NumScaleLevels', 3);
+    I2Corners = I2Corners.selectStrongest( min( 200, length(I2Corners) ));
 
     [featuresI1,validPtsI1] = extractFeatures(I1, I1Corners);
     [featuresI2,validPtsI2] = extractFeatures(I2, I2Corners);
@@ -10,8 +16,10 @@ function [T, outStauts] = findImagesTransformation(I1, I2)
     matchedIndices = matchFeatures(featuresI1, featuresI2);
 %     showMatchedFeatures(I1, I2, validPtsI1(matchedIndices(:,1)), validPtsI2(matchedIndices(:,2)), 'falsecolor');
 
-    [tform,inlierpoints1,inlierpoints2, status] = estimateGeometricTransform(validPtsI2(matchedIndices(:,2)), ...
-        validPtsI1(matchedIndices(:,1)), 'similarity');
+    %[tform,inlierpoints1,inlierpoints2, status] = estimateGeometricTransform(validPtsI2(matchedIndices(:,2)), ...
+    %    validPtsI1(matchedIndices(:,1)), 'projective');
+    [tform,~,~, status] = estimateGeometricTransform(validPtsI2(matchedIndices(:,2)), ...
+        validPtsI1(matchedIndices(:,1)), 'similarity','Confidence',99,'MaxDistance',0.5);
 
     T = tform;
     outStauts = status;
--- final_project/CODE/stabilizeVideo.m
+++ final_project/CODE/stabilizeVideo.m
@@ -1,6 +1,5 @@
 function stabilizeVideo(hObject, handles, inputVideoPath)
-%     firstFrame = step(inputVideo);
-    
+
     inputVideo = VideoReader(inputVideoPath);
     outputVideoPath = fullfile(pwd, '..', '..', 'OUTPUT','stabilized.avi');
     outputVideo = vision.VideoFileWriter(outputVideoPath, 'FrameRate', ...
@@ -8,28 +7,38 @@ function stabilizeVideo(hObject, handles, inputVideoPath)
 
     firstFrame = inputVideo.readFrame();
     grayFirstFrame = rgb2gray(firstFrame);
-    
+    prevFrame = grayFirstFrame;
 %     outputVideo = vision.VideoFileWriter(outputVideo);
+    Tcumulative = projective2d();
+    Tcumulative.T = eye(3);
 
     inputFramesCounter = 0;
     while inputVideo.hasFrame()
         inputFramesCounter = inputFramesCounter + 1;
 %         curFrame = step(inputVideo);
         curFrame = inputVideo.readFrame();
-        [T, status] = findImagesTransformation(grayFirstFrame, rgb2gray(curFrame));
-        
+        %[T, status] = findImagesTransformation(grayFirstFrame, rgb2gray(curFrame));
+        [T, status] = findImagesTransformation(prevFrame, rgb2gray(curFrame));
+
+        Tcumulative.T = T.T * Tcumulative.T;
+
         if status==0 % No error
-            step(outputVideo, imwarp(curFrame, T, 'OutputView', imref2d(size(firstFrame))));                        
+            %step(outputVideo, imgB_RGB, Tcumulative);
+            step(outputVideo, imwarp(curFrame, Tcumulative, 'OutputView', imref2d(size(firstFrame))));
         else
             printMessage(handles, sprintf('skipped frame %d!\n', inputFramesCounter));
         end
-        
+
+        prevFrame = rgb2gray(curFrame);
+
         printMessage(handles, sprintf('Processed %d frames\n', inputFramesCounter));
     end
-    
+
     release(outputVideo);
 %     close(inputVideo);
     printMessage(handles, sprintf('Stabilized video written to OUTPUT/stabilized.avi\n'));
-    
+
+
     setVideoDisplay(hObject, handles, outputVideoPath);
+
 end
