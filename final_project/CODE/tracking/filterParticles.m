52c6ced4a720b96bf1a948d3240d13de96fc3863
Ron <ronll64@gmail.com>
2016-06-24 00:44:14 +0300

    removed an unnecessary comment

--- final_project/CODE/tracking/filterParticles.m
+++ final_project/CODE/tracking/filterParticles.m
@@ -3,10 +3,6 @@ function filtered_S = filterParticles(I,S)
     f = ~(S(3,:) < 1 | S(4,:) < 1 | ...
         S(1,:)-S(3,:) < 1 | S(2,:)-S(4,:) < 1 | ...
         S(1,:)+S(3,:) > width | S(2,:)+S(4,:) > height);
-    
-%     if ~ isempty(find(f==0))
-%         fprintf('filtered out %d particles!\n', length(find(f==0)));
-%     end
 
     filtered_S = S(:, f);
 end
